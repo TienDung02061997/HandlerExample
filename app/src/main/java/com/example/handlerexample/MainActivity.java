8819d8b83bdbbf1ab737facbbcc39579fe6dc740
TienDung02061997 <dungntd00301@fpt.edu.vn>
2019-03-25 08:50:00 +0700

    HandlerExample

--- app/src/main/java/com/example/handlerexample/MainActivity.java
+++ app/src/main/java/com/example/handlerexample/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
-        initViews();
-        listenerHandler();
+            initViews();
+            listenerHandler();
 
     }
 
